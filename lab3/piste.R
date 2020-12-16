1656663105134c2f99dd79d545cf757d3fe4f2c1
student332 <smart.saschka@yandex.ru>
2020-12-16 17:42:07 +0300

    Create piste.R

---
+++ lab3/piste.R
@@ -0,0 +1,10 @@
+install.packages("ggplot2")
+library(ggplot2)
+qplot(data = iris, x = Sepal.Length, y = Petal.Length)
+qplot(data = iris, 
+      x = Sepal.Length,
+      y = Petal.Length,
+      color = Species,
+      size = Petal.Width,
+      alpha = I(0.7))
+
